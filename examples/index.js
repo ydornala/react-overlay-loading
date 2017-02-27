78ddcd601b30344e21c9c015c2f962e99f78aaa1
ydornala <yeshwanth.dornala@objectedge.com>
2017-02-27 13:19:01 +0530

    overlay react loading working

---
+++ examples/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import OverlayLoader from '../lib/OverlayLoader';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            <OverlayLoader loader="ScaleLoader" text="M it's loading..." active={true}>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>
+                      Lorem ipsum Non Excepteur ex non exercitation mollit minim dolor Excepteur qui laborum consequat fugiat proident laborum officia occaecat deserunt dolor laborum in et laboris nostrud minim consequat cillum enim elit.                                        
+                    </td>  
+                  </tr>
+                  <tr>
+                    <td>Lorem ipsum Aute sint et et exercitation sint in veniam cillum enim non irure.</td>
+                  </tr>
+                  <tr>
+                    <td>Lorem ipsum Adipisicing reprehenderit deserunt consequat in ex ullamco officia sed enim est amet non.</td>
+                  </tr>
+                  <tr>
+                    <td>Lorem ipsum Adipisicing reprehenderit deserunt consequat in ex ullamco officia sed enim est amet non.</td>
+                  </tr>                  
+                  <tr>
+                    <td>
+                      Lorem ipsum Non Excepteur ex non exercitation mollit minim dolor Excepteur qui laborum consequat fugiat proident laborum officia occaecat deserunt dolor laborum in et laboris nostrud minim consequat cillum enim elit.                                        
+                    </td>  
+                  </tr>
+                  <tr>
+                    <td>Lorem ipsum Aute sint et et exercitation sint in veniam cillum enim non irure.</td>
+                  </tr>
+                  <tr>
+                    <td>Lorem ipsum Adipisicing reprehenderit deserunt consequat in ex ullamco officia sed enim est amet non.</td>
+                  </tr>
+                  <tr>
+                    <td>Lorem ipsum Adipisicing reprehenderit deserunt consequat in ex ullamco officia sed enim est amet non.</td>
+                  </tr>                                    
+                </tbody>
+              </table>
+            </OverlayLoader>
+         </div>
+      );
+   }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- src/index.js
+++ src/index.js
@@ -1,13 +1,55 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Halogen from 'halogen';
+import styled from 'styled-components';
 
 export default class WrapperComponent extends React.Component {
   render() {
+    let isActive = 'none';
+
+    if(this.props.active) isActive = 'block'
+
+    const Hloader = Halogen[this.props.loader];
+
+    const Wrapper = styled.div`
+      position: relative;
+    `;
+
+    const LoaderWrapper = styled.div`
+      &:before {
+        content: '';
+        background-color: black;
+        width: 100%;
+        height: 100%;
+        opacity: .7;
+        position: absolute;
+      }
+
+      display: ${ isActive }
+    `;    
+
+    const Loader = styled.div`
+      position: absolute;
+      text-align: center;
+      color: white;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    `;
+
+    let textElement = null
+    if (this.props.text) textElement = <div>{this.props.text}</div>
+
     return (
-      <div>
-        <Halogen.PulseLoader color={red} />
-      </div>
+      <Wrapper> 
+        <LoaderWrapper>
+          <Loader>
+            <Hloader color={'grey'} />
+            {textElement}
+          </Loader>
+        </LoaderWrapper>
+        {this.props.children}
+      </Wrapper>
     );
   }
 }
